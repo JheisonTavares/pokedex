670e80e410f7930a2baef7b8a623a91a6a50ecdc
JheisonTavares <133679772+JheisonTavares@users.noreply.github.com>
2024-07-04 06:43:59 -0400

    [fix] renamed an file without snake_case correct

--- lib/features/pokedex/pokedex_route.dart
+++ lib/features/pokedex/pokedex_route.dart
@@ -11,12 +11,21 @@ class PokedexRoute extends StatelessWidget {
     return Navigator(
       initialRoute: '/',
       onGenerateRoute: (settings) {
-        if (settings.name == '/') {}
+        if (settings.name == '/') {
           return MaterialPageRoute(
             builder: (context) {
               return HomeContainer(repository: repository);
             },
           );
+        }
+        return null;
+         /*  if (settings.name == '/details') {
+          return MaterialPageRoute(
+            builder: (context) {
+              return HomeContainer(repository: repository);
+            },
+          );
+        }*/
       },
     );
   }
