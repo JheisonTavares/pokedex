a0a98c08995eb1cc000d56a1b5298f39a754d89c
JheisonTavares <133679772+JheisonTavares@users.noreply.github.com>
2024-07-05 11:03:36 -0400

    [feat] format a code

--- lib/features/pokedex/screens/details/pages/detail_page.dart
+++ lib/features/pokedex/screens/details/pages/detail_page.dart
@@ -4,16 +4,19 @@ class DetailPage extends StatelessWidget {
   const DetailPage({super.key, required this.name});
   final String name;
 
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.brown,
         title: const Text("Details"),
       ),
       body: Center(
-        child: ElevatedButton(onPressed: (){Navigator.pop(context);}, child: const Text("Go Back"),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Go Back"),
         ),
       ),
     );
